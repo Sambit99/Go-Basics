d2ceb4a1633d47fdfb0ff165c10e5d00e277d32a
Sambit99 <sambitkumar75@gmail.com>
2025-06-09 20:00:48 +0530

    feat: add main function to initialize MongoDB API server and set up routing

--- 25-MongoAPI/main.go
+++ 25-MongoAPI/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Sambit99/Go-Basics/MongoAPI/router"
+)
+
+func main() {
+	fmt.Println("MongoDB API")
+	r := router.Router()
+	fmt.Println("Server is getting started...")
+	log.Fatal(http.ListenAndServe(":4000", r))
+	fmt.Println("Listening on port : 4000")
+
+}
