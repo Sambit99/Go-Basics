1f3030101c64ee1a2e02c9a586abb571bb7a39f1
Sambit99 <sambitkumar75@gmail.com>
2025-06-11 20:26:59 +0530

    feat: add mutex locking to goroutines for safe score updates

--- 27-MutexAndAwaitGroups/main.go
+++ 27-MutexAndAwaitGroups/main.go
@@ -8,28 +8,35 @@ import (
 func main() {
 	fmt.Println("Race Condition")
 	wg := &sync.WaitGroup{}
+	mut := &sync.Mutex{}
 
 	var score = []int{0}
 
 	wg.Add(3)
-	go func(wg *sync.WaitGroup) {
+	go func(wg *sync.WaitGroup, mut *sync.Mutex) {
 		fmt.Println("Go Routine No.1")
+		mut.Lock()
 		score = append(score, 1)
+		mut.Unlock()
 		wg.Done()
-	}(wg)
+	}(wg, mut)
 
 	// wg.Add(1)
-	go func(wg *sync.WaitGroup) {
+	go func(wg *sync.WaitGroup, mut *sync.Mutex) {
 		fmt.Println("Go Routine No.2")
+		mut.Lock()
 		score = append(score, 2)
+		mut.Unlock()
 		wg.Done()
-	}(wg)
+	}(wg, mut)
 
-	go func(wg *sync.WaitGroup) {
+	go func(wg *sync.WaitGroup, mut *sync.Mutex) {
 		fmt.Println("Go Routine No.3")
+		mut.Lock()
 		score = append(score, 3)
+		mut.Unlock()
 		wg.Done()
-	}(wg)
+	}(wg, mut)
 
 	wg.Wait()
 
